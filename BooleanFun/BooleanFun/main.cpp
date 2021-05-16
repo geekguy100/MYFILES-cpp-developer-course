6ecce0a74db24b58eb9f882f068439584e167f1e
Kyle Grenier <kyle.grenier11@gmail.com>
2021-05-16 15:21:28 -0500

    Finished boolean lecture

---
+++ BooleanFun/BooleanFun/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	bool isSunny = false;
+	bool isWarm = true;
+
+	cout << boolalpha << "Is Sunny?: " << isSunny << endl;
+	cout << "Is Warm?: " << isWarm << endl;
+
+	getchar();
+	return 0;
+}
