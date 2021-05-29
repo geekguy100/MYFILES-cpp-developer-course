54eaeff9ac0c22b9f6b7a3b3444d6a1507b91b0d
Kyle Grenier <kyle.grenier11@gmail.com>
2021-05-29 01:11:25 -0500

    WeightTracking Revision
    Program can no longer crash when user types in a string instead of a double for the weight.

--- WeightTrackingProject/WeightTrackingProject/Source.cpp
+++ WeightTrackingProject/WeightTrackingProject/Source.cpp
@@ -9,19 +9,40 @@ int main()
 	const int NUM_PEOPLE = 5;
 
 	vector<string> names(NUM_PEOPLE);
+
 	vector<double> weights(NUM_PEOPLE);
-	
+
+
+
 	for (int i = 0; i < NUM_PEOPLE; ++i)
+
 	{
+
 		cout << "Please enter a name: ";
+
 		getline(cin, names[i]);
 
 		cout << "Please enter " << names[i] << "'s weight: ";
+
 		cin >> weights[i];
+
+		while (cin.fail())
+		{
+			cin.clear(); //clear the fail bit
+			cin.ignore(100, '\n');  //ignore a bunch of chars, or up to the \n
+			cout << "Enter a number please!" << endl;
+			cout << "Please enter " << names[i] << "'s weight: ";
+			cin >> weights[i];
+		}//as long as they keep answering with bad input, keep asking
 		cin.get();
+
+
 		cout << endl;
+
 	}
+
 	// Why does inputting a char or string in place of an integer yield such unexpected behaviour?
+
 	cout << "~~~~~~~~~~~~~~~~" << endl << endl;
 
 	for (int i = 0; i < NUM_PEOPLE; ++i)
@@ -30,6 +51,9 @@ int main()
 	}
 
 	cout << endl << endl;
+
 	system("pause");
+
 	return 0;
+
 }
