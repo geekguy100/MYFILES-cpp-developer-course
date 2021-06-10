dcf13a5e2949493cbcae1bb0cfb806dce894abfc
Kyle Grenier <kyle.grenier11@gmail.com>
2021-06-10 00:15:08 -0500

    Basic tic tac toe
    No end game state yet, no edge-case checks

--- TicTacToe/TicTacToe/Source.cpp
+++ TicTacToe/TicTacToe/Source.cpp
@@ -7,15 +7,12 @@ const int COLS = 3;
 
 void drawBoard(char board[ROWS][COLS]);
 void init(char board[ROWS][COLS]);
-void takeTurn(int player);
+void playTurn(bool & xTurn, char board[ROWS][COLS]);
 void gameLoop();
 
 int main()
 {
-	char board[ROWS][COLS];
-	init(board);
-
-	drawBoard(board);
+	gameLoop();
 
 	cout << endl << endl;
 	system("pause");
@@ -41,9 +38,12 @@ void drawBoard(char board[ROWS][COLS])
 		if (i < ROWS - 1)
 			cout << endl << "----------" << endl;
 	}
+
+	cout << endl;
 }
 
-// Initializes each character of the game board to an empty character.
+// Initializes each character of the game board to an empty character,
+// Then draws the board onto the screen.
 void init(char board[ROWS][COLS])
 {
 	for (int i = 0; i < ROWS; ++i)
@@ -53,14 +53,64 @@ void init(char board[ROWS][COLS])
 			board[i][j] = '\0';
 		}
 	}
+
+	drawBoard(board);
 }
 
-void takeTurn(int player)
+// Gets the player's input.
+void playTurn(bool & xTurn, char board[ROWS][COLS])
 {
+	char playingChar;
 
+	if (xTurn)
+	{
+		cout << "It is X's turn." << endl;
+		playingChar = 'X';
+	}
+	else
+	{
+		cout << "It is O's turn." << endl;
+		playingChar = 'O';
+	}
+
+	cout << "Please enter the row THEN the column, each from 0, 1, or 2, separated by a space." << endl;
+	int row{};
+	int col{};
+
+	cin >> row;
+	cin >> col;
+
+	// TODO: Deal with invalid input.
+	
+	// If the spot is already occupied, notify the player
+	// and keep requesting a valid input.
+	while (board[row][col] != '\0')
+	{
+		cout << "That location is already occupied." << endl;
+		cout << "Please enter the row THEN the column, each from 0, 1, or 2, separated by a space." << endl;
+
+		cin >> row;
+		cin >> col;
+	}
+
+	cout << endl << endl;
+
+	board[row][col] = playingChar;
+	xTurn = !xTurn;
 }
 
+// The core game loop. Executes all of the important game mechanics.
 void gameLoop()
 {
+	char board[ROWS][COLS];
+	bool xTurn = true;
+	bool gameOver = false;
 
+	init(board);
+
+	while (!gameOver)
+	{
+		playTurn(xTurn, board);
+		drawBoard(board);
+	}
 }
